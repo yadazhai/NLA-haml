56055633415ca82cbb36174749ff77b49aa5d1a4
Yada Zhai <yadazhai@gmail.com>
2013-09-23 13:35:28 -0400

    bulk imports navbar fixed

--- app/controllers/bulk_imports_controller.rb
+++ app/controllers/bulk_imports_controller.rb
@@ -8,7 +8,7 @@ class BulkImportsController < ApplicationController
     @bulk_import.file = params[:bulk_import][:file]
     # @bulk_import.all_valid = true
     if @bulk_import.save
-      redirect_to '/', notice: "Imported products successfully."
+      redirect_to students_path, notice: "Imported products successfully."
     else
       render :step1
     end
